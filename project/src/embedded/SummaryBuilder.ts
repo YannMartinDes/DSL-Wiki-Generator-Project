c608dd1061a4300d06bf245a1cb0a395008a6477
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2022-02-15 22:18:39 +0100

    Summary Builder

--- project/src/embedded/SummaryBuilder.ts
+++ project/src/embedded/SummaryBuilder.ts
@@ -1,21 +1,57 @@
 import Summary from "../kernel/models/summary";
-import ElementBuilder from "./wikiElementBuilder";
 import SubjectBuilder from "./SubjectBuilder";
+import { BUTTON, IMAGE, TABLE, TEXT } from "./Const";
+import WikiElementBuilder from "./wikiElementBuilder";
+import WikiElement from "../kernel/models/elements/wikiElement";
+import Linkable from "../kernel/models/linkable";
 
 class SummaryBuilder {
 
     private rootBuilder:SubjectBuilder;
     public name:string;
-    private elements:ElementBuilder[];
+    private elements:WikiElementBuilder[];
 
     constructor(rootBuilder:SubjectBuilder, name:string) {
         this.name = name;
         this.rootBuilder = rootBuilder;
         this.elements = []
     }
-    
-    createModel():Summary {
-        return new Summary();
+
+    addText(id:string){
+        const builder = new WikiElementBuilder(this,id,TEXT);
+        this.elements.push(builder);
+        return builder;
+    }
+
+    addTable(id:string){
+        const builder = new WikiElementBuilder(this,id,TABLE);
+        this.elements.push(builder);
+        return builder;
+    }
+
+    addImage(id:string){
+        const builder = new WikiElementBuilder(this,id,IMAGE);
+        this.elements.push(builder);
+        return builder;
+    }
+
+    addButton(id:string){
+        const builder = new WikiElementBuilder(this,id,BUTTON);
+        this.elements.push(builder);
+        return builder;
+    }
+
+    root(){
+        return this.rootBuilder;
+    }
+
+    createModel(linkableHM:Map<string,Linkable>):Summary {
+
+        const elementsList:WikiElement[] = []
+        for(const el of this.elements){
+            elementsList.push(el.createModel(linkableHM))
+        }
+        return new Summary(elementsList);
     }
 }
 export default SummaryBuilder
