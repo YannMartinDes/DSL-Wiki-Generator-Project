4dfa48844c02892a399c0e29e08c39b9835122be
Mentra20 <yann.martindes@orange.fr>
2022-02-15 22:30:56 +0100

    add sectionBuilder

--- project/src/kernel/models/section.ts
+++ project/src/kernel/models/section.ts
@@ -2,5 +2,10 @@ import WikiElement from "./elements/wikiElement";
 import Linkable from "./linkable";
 
 export default class Section extends Linkable{
-    elements?:WikiElement[];
+    elements:WikiElement[];
+
+    constructor(elements:WikiElement[]){
+        super();
+        this.elements = elements;
+    }
 }
