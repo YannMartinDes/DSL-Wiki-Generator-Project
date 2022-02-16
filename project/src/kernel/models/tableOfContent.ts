31ccd425242a936cf3d8bbf7f89a48778a1b0eac
LucieMorant <lucie.morant@etu.univ-cotedazur.fr>
2022-02-16 11:45:00 +0100

    Update TableOfContent

--- project/src/kernel/models/tableOfContent.ts
+++ project/src/kernel/models/tableOfContent.ts
@@ -1,8 +1,12 @@
+import TextStyle from "../textStyle";
 import WikiElement from "./elements/wikiElement";
 
 export default class TableOfContent {
     elements:WikiElement[];
     id:string;
+    title?:string;
+    titleStyle?:TextStyle;
+    numbered?:boolean
 
     constructor(id:string,elements:WikiElement[]){
         this.elements = elements;
