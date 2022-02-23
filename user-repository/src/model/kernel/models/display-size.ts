03c6ba9be7d3e0248784b6143c988e211f4d5b8f
Yohann <tognetti.yohann@outlook.fr>
2022-02-23 10:12:16 +0100

    ajout de la gestion de la taille de l'ecran dans le model

---
+++ user-repository/src/model/kernel/models/display-size.ts
@@ -0,0 +1,12 @@
+export class DisplaySize<T>{
+    minWidth=-1
+    minHeight=-1
+    maxHeight=-1
+    maxWidth=-1
+    element:T
+    constructor(element:T){
+        this.element=element
+    }
+    
+
+}
