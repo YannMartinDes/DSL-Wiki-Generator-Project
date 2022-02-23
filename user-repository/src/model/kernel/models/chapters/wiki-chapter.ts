3935e919efce77616ef314d63fa7817805f45d06
Mentra20 <yann.martindes@orange.fr>
2022-02-23 10:30:13 +0100

    delete bibliography

--- user-repository/src/model/kernel/models/chapters/wiki-chapter.ts
+++ user-repository/src/model/kernel/models/chapters/wiki-chapter.ts
@@ -1,7 +1,6 @@
 import WikiTextStyle from "../style/wiki-text-style";
 import WikiBlockStyle from "../style/wiki-block-style";
 import WikiGallery from "./wiki-gallery";
-import WikiBibliography from "./wiki-bibliography";
 import WikiClassicChapter from "./wiki-classic-chapter";
 import WikiElement from "../elements/wiki-element";
 
@@ -13,18 +12,16 @@ export default class WikiChapter{
     titleStyle?:WikiTextStyle;
 
     gallery?:WikiGallery;
-    bibliography?:WikiBibliography;
     classicChapter?:WikiClassicChapter;
 
-    constructor({subChapter, content, block, title, gallery, bibliography, classicChapter}:
+    constructor({subChapter, content, block, title, gallery, classicChapter}:
         {subChapter?:WikiChapter, content?:WikiElement, block?:WikiBlockStyle, title?:WikiTextStyle,
-        gallery?:WikiGallery, bibliography?:WikiBibliography, classicChapter?:WikiClassicChapter}){
+        gallery?:WikiGallery, classicChapter?:WikiClassicChapter}){
         this.subChapter = subChapter;
         this.blockStyle = block;
         this.titleStyle = title;
         this.gallery = gallery;
         this.contentStyle =content;
-        this.bibliography =bibliography;
         this.classicChapter = classicChapter;
     }
 }
