f75a5b581f3dfeeeba79e08a61b4a54319e54d6f
Yohann <tognetti.yohann@outlook.fr>
2022-02-26 14:27:01 +0100

    redirection automatique vers le premier article

--- wiki-react/src/components/wiki-component.tsx
+++ wiki-react/src/components/wiki-component.tsx
@@ -1,22 +1,15 @@
-import { useEffect } from 'react'
-import { Navigate, Route, Routes, useLocation, useNavigate } from 'react-router-dom'
+import { Navigate, Route, Routes} from 'react-router-dom'
 import { SubjectContent, WikiContent } from '../model/kernel/models/content'
 import NavBarComponent from './nav-bar-component'
 import SubjectComponent from './subject-component'
 
 export default function WikiComponent({content}:{content:WikiContent}) {
-  // const navigate = useNavigate();
-  // const location = useLocation()
-  // useEffect(()=>{
-  //   navigate(content.subjects[0].url)
-  //   console.log("navigate to "+content.subjects[0].url)
-  //   console.log(location.pathname)
-  // },[])
 
   return (
     <div className='wiki'>
       {content.navBar?<NavBarComponent content={content.navBar}/>:undefined}
       <Routes>
+        <Route path="/" element={<Navigate to={content.subjects[0].url}></Navigate>}></Route>
         {content.subjects.map((subject:SubjectContent,index:number)=>{console.log(subject.url);return <Route key={index} path={subject.url} element={<SubjectComponent content={subject}/>}/>})}
       </Routes>
     </div>
